9b064a2f7f7db77ca9ec7c854a86c8ee05adc7ac
theconsigliere <hello@maxwellkirwin.co.uk>
2025-11-01 01:09:21 +0000

    feat: button control added

--- src/lib/components/tuningControl/CurrentTuning.svelte
+++ src/lib/components/tuningControl/CurrentTuning.svelte
@@ -1,12 +1,37 @@
 <script lang="ts">
     import { musicKeys } from "$lib/data/musicKeys";
     import { setCurrentTuningArray, selectedTuning } from "$lib/tunerStore";
+    import TuningButtonControl from "./TuningButtonControl.svelte";
+    import { page } from '$app/state';
 </script>
 
-<div class="current-tuning-container">
-    <p>current tuning: component</p>
-    {#each $setCurrentTuningArray as tuning, index}
-        <div>{tuning}</div>
-    {/each}
-    <p>current tuning label: {$selectedTuning}</p>
+<div class="current-tuning">
+    <div class="current-tuning__container">
+        {#each $setCurrentTuningArray as tuning, index}
+        <div>   
+            <button class="h1">{tuning}</button>
+
+            <!--  only show TuningButtonControl on /tuner page -->
+            {#if page.url.pathname === '/tuner'}
+              <TuningButtonControl {tuning} />
+            {/if}
+        </div>
+        {/each}
+    </div>
+    <p>{$selectedTuning}</p>
  </div>  
+
+ <style>
+    .current-tuning {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+    }
+
+    .current-tuning__container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+</style>
---
+++ src/lib/components/tuningControl/TuningButtonControl.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    // import { selectedTuning } from "$lib/tunerStore";
+
+    export let tuning: string;
+
+    // function handleClick() {
+    //     selectedTuning.set(tuning);
+    // }
+</script>
+
+<div class="tuning-button__info {tuning}">
+    <button>+</button>
+    <button>-</button>
+</div>
